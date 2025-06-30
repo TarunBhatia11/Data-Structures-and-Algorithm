21838cb343670020f1396b9570e8bf9d2ee34ab3
TARUN BHATIA <155810253+TarunBhatia11@users.noreply.github.com>
2025-06-30 19:51:37 +0530

    Create solution01.cpp

---
+++ Day03/solution01.cpp
@@ -0,0 +1,26 @@
+// User function template for C++
+class Solution {
+  public:
+    int longestSubarrayDivK(vector<int>& arr, int k) {
+        // Complete the function
+        int n = arr.size();
+        int maxL = 0;
+        int sum = 0;
+        unordered_map<int,int> map;
+        
+        for(int i=0; i<n; i++){
+            
+            sum = ((sum + arr[i]) % k + k)%k;
+            
+            if(sum == 0){
+                maxL = i+1;
+            }
+            else if(map.find(sum) != map.end()){
+                maxL = max(maxL,i-map[sum]);
+            }else{
+                map[sum] = i;
+            }
+        }
+        return maxL;
+    }
+};
