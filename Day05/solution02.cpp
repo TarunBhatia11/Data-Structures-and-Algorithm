06065e1ede028c9a6dd834ee72777e1207b00ea5
TARUN BHATIA <155810253+TarunBhatia11@users.noreply.github.com>
2025-07-02 21:51:31 +0530

    Create solution02.cpp

---
+++ Day05/solution02.cpp
@@ -0,0 +1,44 @@
+// User function template for C++
+class Solution {
+  public:
+    int search(string &pat, string &txt) {
+        // code here
+        vector<int>hashCurr(26,0);
+        vector<int>hashGiven(26,0);
+        
+        int n = txt.size();
+        int i = 0;
+        int j  = 0;
+        int k = pat.length();
+        int noOfAnagram = 0;
+        
+        for(auto elem : pat)
+        {
+            hashGiven[elem-'a']++;
+        }
+        
+        while(j<n)
+        {
+            // calculation
+            hashCurr[txt[j]-'a']++;
+        
+            if(j-i+1 < k) ++j;
+            
+            // window size hit
+            else 
+            {
+                // conclusion
+                if(hashCurr == hashGiven)
+                {
+                    noOfAnagram++;
+                }
+                // slide the window
+                hashCurr[txt[i]-'a']--;
+                ++i;
+                ++j;
+            }
+        }
+        
+        return noOfAnagram;
+    }
+};
